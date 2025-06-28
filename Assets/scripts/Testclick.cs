dd34441bd87a34042514ab51acc99a35eeece4e8
liuxy039 <159973978+liuxy039@users.noreply.github.com>
2025-06-28 10:13:02 +0800

    提交
    123

---
+++ Assets/scripts/Testclick.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testclick : MonoBehaviour
+{
+    private bool isalive=false;
+    private void OnMouseDown()
+    {
+        // 点击后执行的行为
+        Debug.Log("物体被点击了！");
+        isalive=true; 
+
+
+    }
+
+    private void Update()
+    {
+        if(isalive)
+        {
+            Debug.Log(Time.time);
+        }
+    }
+}
